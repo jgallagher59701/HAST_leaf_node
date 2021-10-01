5dd5ed5437a9722705dd5416ee3a6662a94c8268
James Gallagher <jgallagher@opendap.org>
2021-09-30 22:22:08 -0600

    After re-loading the bootloader, the RS works again.
    I removed various hacks I tried before realizing it was the
    hosed bootloader.

--- src/leaf_node_main.cc
+++ src/leaf_node_main.cc
@@ -10,24 +10,33 @@
 #define Serial SerialUSB // Needed for RS. jhrg 7/26/20
 
 void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
 
+    delay(5000);
+
+#if 0
     get_battery_voltage_setup();
-#if 1
+
 
     Serial.begin(115200);
-    while (!Serial)
-        ;
-
+    //while (!Serial)
+    //  ;
+    //yield();
 
     Serial.println("Begin...");
 #endif
 }
 
 void loop() {
-#if 1
+#if 0
     Serial.print("get_battery_voltage(): ");
     Serial.println(get_battery_voltage());
-#endif
+
     get_battery_voltage();
     delay(1000);
+#endif
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
 }
